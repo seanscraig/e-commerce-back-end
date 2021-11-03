c4ae719fa98ca3028a4d60460c005592e1f3f0b3
Sean Craig <seancraig21@gmail.com>
2021-11-03 18:48:50 -0500

    tag-routes put request is working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,8 +46,27 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
@@ -58,7 +77,7 @@ router.delete("/:id", async (req, res) => {
     });
 
     if (!tagData) {
-      res.status(404).json({ message: 'No tag found with that id!' });
+      res.status(404).json({ message: "No tag found with that id!" });
       return;
     }
 
