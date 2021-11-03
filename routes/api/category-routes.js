6d327cc489ec7c9928bbcca2ddb71d84d2207fa2
Sean Craig <seancraig21@gmail.com>
2021-11-03 18:44:31 -0500

    added sequelize to server and categories put works

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,11 +48,15 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   // update a category by its `id` value
-  // NOT WORKING
   try {
-    const categoryData = await Category.update(req.body, {
-      where: { id: req.params.id },
-    });
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
 
     if (!categoryData) {
       res.status(404).json({ message: "No category found with this id!" });
